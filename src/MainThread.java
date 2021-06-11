5782f86f6aed5bb39be51bf80be079c1069932f2
nmq126 <minhquangngo.98@gmail.com>
2021-06-11 20:51:16 +0700

    last commit

--- src/MainThread.java
+++ src/MainThread.java
@@ -1,2 +1,33 @@
+import entity.HelperClass;
+
+import java.util.Scanner;
+
 public class MainThread {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        HelperClass helperClass = new HelperClass();
+        // Lab 1
+        double luongThang13 = helperClass.tinhLuongThang13(2, 1);
+        System.out.println("Lương tháng 13 là " + luongThang13 + " triệu");
+        // Lab 1.2
+        System.out.println("Nhập vào độ dài 3 cạnh của tam giác: ");
+        System.out.println("Cạnh 1: ");
+        double a = scanner.nextDouble();
+        System.out.println("Cạnh 2: ");
+        double b = scanner.nextDouble();
+        System.out.println("Cạnh 3: ");
+        double c = scanner.nextDouble();
+        if (a <= 0 || b <= 0 || c <= 0) {
+            System.out.println("Độ dài một cạnh tam giác không thể nhỏ hơn hoặc bằng 0");
+            return;
+        } else if (a + b < c || a + c < b || b + c < a) {
+            System.out.println("Tổng hai cạnh của một tam giác luôn phải lớn hơn cạnh còn lại (Theo bất đẳng thức tam giác)");
+            return;
+        } else {
+            System.out.println("Nửa chu vui tam giác là " + helperClass.tinhNuaChuViTamGiac(a, b, c));
+        }
+        // Lab 1.3
+        int[] array = {12, 15, 0, 35, 4, 9};
+        helperClass.timMinMax(array);
+    }
 }
--- src/entity/HelperClass.java
+++ src/entity/HelperClass.java
@@ -1,4 +1,46 @@
 package entity;
 
 public class HelperClass {
+    public double tinhLuongThang13(int a, int b){
+        double luong = 0, thuong = 0;
+        if (a < 2){
+            luong = 10;
+        }
+        else if (2 <= a && a <= 5){
+            luong = 20;
+        }
+        else {
+            luong = 30;
+        }
+        if (b < 1){
+            thuong = luong * 0.3;
+        }
+        else if (1 <= b && b <= 2){
+            thuong = luong * 0.5;
+        }
+        else if (2 <= b && b <= 5){
+            thuong = luong;
+        }
+        else thuong = luong * 2;
+        return  thuong;
+    }
+    public double tinhNuaChuViTamGiac(double a, double b, double c){
+        return (double) (a + b + c) / 2;
+    }
+    public void timMinMax(int[] a){
+        int max = a[0];
+        for (int i = 0; i < a.length; i++){
+            if (a[i] > max){
+                max = a[i];
+            }
+        }
+        System.out.println("Số nguyên lớn nhất: " + max);
+        int min = a[0];
+        for (int i = 0; i < a.length; i++){
+            if (a[i] < min){
+                min = a[i];
+            }
+        }
+        System.out.println("Số nguyên nhỏ nhất: " + min);
+    }
 }
